ff379445cf54291779be7bb531f6c87e726f9ffa
VvsJaemin <djd453216@nate.com>
2023-02-19 23:29:54 +0900

    프로젝트 구성

---
+++ fe/src/index.tsx
@@ -0,0 +1,17 @@
+// src/index.tsx
+import React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+
+const container = document.getElementById('root') as HTMLElement
+const root = ReactDOM.createRoot(container)
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
+
+reportWebVitals()
