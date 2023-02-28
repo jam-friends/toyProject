caf27fec4272924f67630fb34179a8bfa8d5263e
eve <eve@gna.company>
2023-02-28 20:04:33 +0900

    delete fe

--- fe/src/pages/api/getListData.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { cookie } = req.headers;
-  res.status(200).json({});
-}
--- fe/src/pages/listPage.tsx
+++
@@ -1,5 +0,0 @@
-export default function ListPage() { 
-  return (
-    <>ListPage</>
-  )
-}
