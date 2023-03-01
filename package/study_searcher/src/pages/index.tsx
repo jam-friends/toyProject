d6f7c94c47aee100531dd54a29df4857149b39d2
Jinwook <wlsdnr4675@gmail.com>
2023-03-01 20:22:34 +0900

    emotion and tailwind css

--- package/study_searcher/src/pages/index.tsx
+++ package/study_searcher/src/pages/index.tsx
@@ -7,15 +7,6 @@ export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>{' '}
-      <ExampleComponentCSS>fdsfdsfdsfds</ExampleComponentCSS>
-      <ExampleComponentCSS2>fdsfdsfdsfds</ExampleComponentCSS2>
     </>
   )
 }
-
-const ExampleComponentCSS = styled.div`
-  color: red;
-`
-const ExampleComponentCSS2 = tw.div`
-bg-amber-50
-`
